b1cb05ce2c844395288210c1cd753df4ecb30a35
zachary <liuzhenm@mail.ustc.edu.cn>
2021-01-18 15:02:56 +0800

    add build compile commands shell scripts for better vim auto complete(coc-clangd)

--- tests/config_reader_test.cc
+++ tests/config_reader_test.cc
@@ -1,12 +1,28 @@
+#include "yaml-cpp/emitter.h"
+#include "yaml-cpp/emittermanip.h"
 #include "yaml-cpp/yaml.h"
 #include "gtest/gtest.h"
 
-TEST(YAMLTest, Hello){
+TEST(YAMLTest, TestHello){
    YAML::Emitter out;
    out << "Hello, World!";
-   std::cout << "Here's the output YAML:\n" << out.c_str(); // prints "Hello, World!"
-   ASSERT_EQ(0,0);
+   std::string expectedStr = "Hello, World!";
+   ASSERT_STREQ(expectedStr.c_str(), out.c_str());
 }
+
+TEST(TAMLTest, TestMap){
+  YAML::Emitter out;
+  out << YAML::BeginMap;
+  out << YAML::Key << "name";
+  out << YAML::Value << "Zach";
+  out << YAML::Key << "position";
+  out << YAML::Value << "LF";
+  out << YAML::EndMap;
+  std::string expectedStr;
+  expectedStr = "name: Zach\nposition: LF";
+  ASSERT_STREQ(expectedStr.c_str(), out.c_str());
+}
+
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
