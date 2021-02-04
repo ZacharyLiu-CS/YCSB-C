f312e6aa1a32388e5c27dec92a5e1b8297f3c6ef
ZacharyLiu-CS <liuzhenm@mail.ustc.edu.cn>
2021-02-04 15:57:38 +0000

    fix build third-party run path error

--- third_party/build_third_party.sh
+++ third_party/build_third_party.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cd ./googletest && mkdir -p build && cd build && cmake .. && make -j
-cd ../..
-cd ./yaml-cpp && mkdir -p build && cd build && cmake .. && make -j
+PROJ_PATH=$(pwd)
+PROJ_PATH=${PROJ_PATH%YCSB-C*}YCSB-C
+cd ${PROJ_PATH}/third_party/googletest && mkdir -p build && cd build && cmake .. && make -j
+cd ${PROJ_PATH}/third_party/yaml-cpp && mkdir -p build && cd build && cmake .. && make -j
 
 
