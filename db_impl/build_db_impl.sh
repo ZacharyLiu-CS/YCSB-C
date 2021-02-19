541db7b8ab79e4224c105ccf8399767a1c7daeaa
liuzhen <liuzhenm@mail.ustc.edu.cn>
2021-02-19 22:49:34 +0800

    restrict cpu resource usage in order to avoid system breakdown

--- db_impl/build_db_impl.sh
+++ db_impl/build_db_impl.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 PROJ_PATH=$(pwd)
-cd ${PROJ_PATH}/leveldb && mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build . -j
-cd ${PROJ_PATH}/rocksdb && mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build . -j
+CORE_NUM=cat /proc/cpuinfo| grep "cpu cores"| uniq
+cd ${PROJ_PATH}/leveldb && mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build . -j ${CORE_NUM}
+cd ${PROJ_PATH}/rocksdb && mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build . -j ${CORE_NUM}
+
 
 
