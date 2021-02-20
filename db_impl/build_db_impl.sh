c1c9457424def7e4b2e6018eba807b5a84ddbb44
ZacharyLiu-CS <liuzhenm@mail.ustc.edu.cn>
2021-02-20 13:14:50 +0800

    change rocksdb branch from 6.4 to 6.7, try to fix github action errors.

--- db_impl/build_db_impl.sh
+++ db_impl/build_db_impl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PROJ_PATH=$(pwd)
-CORE_NUM=cat /proc/cpuinfo| grep "cpu cores"| uniq
+CORE_NUM=$((`nproc`-1))
+echo "cpu cores: $CORE_NUM"
 cd ${PROJ_PATH}/leveldb && mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build . -j ${CORE_NUM}
 cd ${PROJ_PATH}/rocksdb && mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build . -j ${CORE_NUM}
 
