c2e48ef7748d9e5792f9acfdb83d3b359af2fcb9
dssdb <dssdb@skv-node4>
2022-07-11 17:41:11 +0800

    add pre compile command to make sure everything is ready

--- third_party/build_third_party.sh
+++ third_party/build_third_party.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-PROJ_PATH=$(pwd)
+PROJ_PATH=$(dirname "$0")
+PROJ_PATH=$(realpath $PROJ_PATH)
+echo "Running into: "$PROJ_PATH
 cd ${PROJ_PATH}/googletest && mkdir -p build && cd build && cmake .. && make -j
 cd ${PROJ_PATH}/yaml-cpp && mkdir -p build && cd build && cmake .. && make -j
 
