03af37fc96fcf1e8b51fc364321efd8a2556aa35
wxy <wxy1999>
2022-08-07 15:38:07 +0800

    switch to a lru version db

--- db_impl/build_db_impl.sh
+++ db_impl/build_db_impl.sh
@@ -4,7 +4,7 @@ PROJ_PATH=$(realpath $PROJ_PATH)
 echo "Running into: "$PROJ_PATH
 CORE_NUM=$((`nproc`-1))
 echo "cpu cores: $CORE_NUM"
-cd ${PROJ_PATH}/codebase && git checkout remote_meory && git pull 
+cd ${PROJ_PATH}/codebase && git checkout cache_base && git pull 
 cd ${PROJ_PATH}/codebase && mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build . -j ${CORE_NUM}
 
 
